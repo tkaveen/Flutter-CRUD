85ee7cceccf85c9cf9bad3feb2aa7afce96ebc11
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-08-16 10:22:32 +0530

    UI improved

--- lib/todoList.dart
+++ lib/todoList.dart
@@ -2,7 +2,13 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class TodoList extends StatelessWidget {
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  bool isComplete = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +25,25 @@ class TodoList extends StatelessWidget {
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
               ),
-              Divider(),
+              Divider(
+                color: Colors.grey[100],
+              ),
               SizedBox(
                 height: 20,
               ),
-              ListView.builder(
+              ListView.separated(
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.grey[200],
+                ),
                 shrinkWrap: true,
                 itemCount: 5,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    onTap: () {},
+                    onTap: () {
+                      setState(() {
+                        isComplete = !isComplete;
+                      });
+                    },
                     leading: Container(
                       padding: EdgeInsets.all(2),
                       height: 30,
@@ -36,10 +51,12 @@ class TodoList extends StatelessWidget {
                       decoration: BoxDecoration(
                           color: Theme.of(context).primaryColor,
                           shape: BoxShape.circle),
-                      child: Icon(
-                        Icons.check,
-                        color: Colors.white,
-                      ),
+                      child: isComplete
+                          ? Icon(
+                              Icons.check,
+                              color: Colors.white,
+                            )
+                          : Container(),
                     ),
                     title: Text(
                       "Todo title",
@@ -62,19 +79,55 @@ class TodoList extends StatelessWidget {
           showDialog(
               context: context,
               child: SimpleDialog(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                backgroundColor: Colors.grey[800],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
                 title: Row(
                   children: [
-                    Text("Add Todo"),
+                    Text(
+                      "Add Todo",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
                     Spacer(),
                     IconButton(
                       icon: Icon(
                         Icons.cancel,
                         color: Colors.grey,
+                        size: 30,
                       ),
-                      onPressed: () {},
+                      onPressed: () => Navigator.pop(context),
                     )
                   ],
                 ),
+                children: [
+                  Divider(),
+                  TextFormField(
+                    style: TextStyle(
+                        fontSize: 18, height: 1.5, color: Colors.white),
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        hintText: "eg. Go to gym",
+                        hintStyle: TextStyle(color: Colors.white70),
+                        border: InputBorder.none),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text("Add"),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      onPressed: () {},
+                    ),
+                  )
+                ],
               ));
         },
       ),
