8b3c93ba9b0439e9c6aff4a134b1a8d33f7d2118
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-08-16 12:36:02 +0530

    Database Connected

--- lib/todoList.dart
+++ lib/todoList.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
 
+import 'package:crudapp/loading.dart';
+import 'package:crudapp/model/todo.dart';
+import 'package:crudapp/services/database_services.dart';
 import 'package:flutter/material.dart';
 
 class TodoList extends StatefulWidget {
@@ -14,75 +17,82 @@ class _TodoListState extends State<TodoList> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "All Todos",
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              Divider(
-                color: Colors.grey[100],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ListView.separated(
-                separatorBuilder: (context, index) => Divider(
-                  color: Colors.grey[200],
-                ),
-                shrinkWrap: true,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                      key: Key(index.toString()),
-                      background: Container(
-                        padding: EdgeInsets.only(left: 20),
-                        alignment: Alignment.centerLeft,
-                        child: Icon(Icons.delete),
-                        color: Colors.red,
+        child: StreamBuilder<List<Todo>>(
+            stream: DatabaseService().listTodos(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Loading();
+              }
+              return Padding(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "All Todos",
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Divider(
+                      color: Colors.grey[100],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ListView.separated(
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.grey[200],
                       ),
-                      onDismissed: (direction) {
-                        print("Removed");
+                      shrinkWrap: true,
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                            key: Key(index.toString()),
+                            background: Container(
+                              padding: EdgeInsets.only(left: 20),
+                              alignment: Alignment.centerLeft,
+                              child: Icon(Icons.delete),
+                              color: Colors.red,
+                            ),
+                            onDismissed: (direction) {
+                              print("Removed");
+                            },
+                            child: ListTile(
+                              onTap: () {
+                                setState(() {
+                                  isComplete = !isComplete;
+                                });
+                              },
+                              leading: Container(
+                                padding: EdgeInsets.all(2),
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).primaryColor,
+                                    shape: BoxShape.circle),
+                                child: isComplete
+                                    ? Icon(
+                                        Icons.check,
+                                        color: Colors.white,
+                                      )
+                                    : Container(),
+                              ),
+                              title: Text(
+                                "Todo title",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.grey[200],
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ));
                       },
-                      child: ListTile(
-                        onTap: () {
-                          setState(() {
-                            isComplete = !isComplete;
-                          });
-                        },
-                        leading: Container(
-                          padding: EdgeInsets.all(2),
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColor,
-                              shape: BoxShape.circle),
-                          child: isComplete
-                              ? Icon(
-                                  Icons.check,
-                                  color: Colors.white,
-                                )
-                              : Container(),
-                        ),
-                        title: Text(
-                          "Todo title",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.grey[200],
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ));
-                },
-              )
-            ],
-          ),
-        ),
+                    )
+                  ],
+                ),
+              );
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -137,9 +147,10 @@ class _TodoListState extends State<TodoList> {
                       child: Text("Add"),
                       color: Theme.of(context).primaryColor,
                       textColor: Colors.white,
-                      onPressed: () {
+                      onPressed: () async {
                         if (todoTitleController.text.isNotEmpty) {
-                          print(todoTitleController.text);
+                          await DatabaseService()
+                              .createNewTodo(todoTitleController.text.trim());
                           Navigator.pop(context);
                         }
                       },
